811c1d6a11cebbb3752f7068845c14e7ef8e1b9d
ko4an <eechor1@mail.ru>
2019-04-27 18:57:30 +0300

    start project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+      $('.main_slider').slick({
+      	dots:true,
+      	infinite: true,
+        slidesToShow: 1,
+ 		slidesToScroll: 1,
+  		autoplay: true,
+  		autoplaySpeed: 15000,
+  		fade: true,
+  		cssEase: 'linear'
+      });
+    });
